1e05fca7835d9215fff99e3354b823a825d0b6b9
J Mieras <jmieras@gmail.com>
2020-05-16 22:33:05 -0700

    new index page using radio buttons

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,7 @@ function tallyResults(){
   var dGo = 0;
   var eCs = 0;
 
-  var questionAnswer; 
+  var questionAnswer;
   var questions = document.getElementsByClassName("quizQuestion");
   var questionsTotal = 5;
 
@@ -17,14 +17,14 @@ function tallyResults(){
       }
     else if (questionAnswer === "b") {
       bPython++;
-      } 
+      }
     else if (questionAnswer === "c") {
       cJS++;
-      } 
+      }
     else if (questionAnswer === "d")  {
       dGo++;
       }
-    else if (questionAnswer === "e")  {
+    else (questionAnswer === "e")  {
       eCs++;
       }
    }
@@ -32,53 +32,55 @@ function tallyResults(){
    var results = [aRuby, bPython, cJS, dGo, eCs];
    results.sort();
    results.reverse();
-   var rank1 = results[0];
-   var rank2 = results[1];
-   var rank3 = results[2];
+   let rank1 = results[0];
+   let rank2 = results[1];
+   let rank3 = results[2];
 
 }
 
 
 $(document).ready(function() {
-
   $("form#questions").submit(function(){
+        let rank1 = "none"
+        let rank2 = "none"
+        let rank3 = "none"
       $("#quizQuestion").tallyResults() {
-        if (rank1 || rank2 || rank3  === aRuby) {
+
+        if ((rank1 || rank2 || rank3) === aRuby) {
           ("#ruby").show();
         }
-        else if (rank1 || rank2 || rank3  === bPython) {
+        else if ((rank1 || rank2 || rank3) === bPython) {
           ("#python").show();
         }
-        else if (rank1 || rank2 || rank3  === cJS) {
+        else if ((rank1 || rank2 || rank3) === cJS) {
           ("#js").show();
         }
-        else if (rank1 || rank2 || rank3  === dGo) {
+        else if ((rank1 || rank2 || rank3) === dGo) {
           ("#go").show();
         }
-        else if (rank1 || rank2 || rank3  === eCs) {
+        else if ((rank1 || rank2 || rank3) === eCs) {
           ("#cs").show();
         }
         else {
           alert("Something went wrong!")
         }
-     }
-    });
+      }
 
-    $("form#blanks").submit(function() {
-      var blanks = ["userName"];
 
-      blanks.forEach(function(blank) {
-        var userInput = $("input#" + blank).val();
-        $("." + blank).text(userInput).val();      
+    // $("form#blanks").submit(function() {
+    //   var blanks = ["userName"];
 
-    });
+    //   blanks.forEach(function(blank) {
+    //     var userInput = $("input#" + blank).val();
+    //     $("." + blank).text(userInput).val();
+    //    });
+    // });
 
-    event.preventDefault();
-
-  $("button").click(function() {
-    $("." + blanks).toggle();
-    $("." + results).toggle();
+  // $("btn").click(function() {
+  //   $("#blanks").toggle();
+  //   $("#results").toggle();
   });
-  
+
+  // event.preventDefault();
 
 });
---
+++ scripts2.js
@@ -0,0 +1,120 @@
+
+function tallyResults() {
+  var aRuby = 0;
+  var bPython = 0;
+  var cJS = 0;
+  var dGo = 0;
+  var eCs = 0;
+
+  var questionAnswer;
+  var questions = document.getElementsByClassName("quizQuestion");
+  var questionsTotal = 5;
+
+  for (var i = 0; i < questionsTotal; i++) {
+    questionAnswer = questions[i].options[questions[i].selectedIndex].value;
+    if (questionAnswer === "a") {
+      aRuby++;
+    }
+    else if (questionAnswer === "b") {
+      bPython++;
+    }
+    else if (questionAnswer === "c") {
+      cJS++;
+    }
+    else if (questionAnswer === "d") {
+      dGo++;
+    }
+    else if (questionAnswer === "e") {
+      eCs++;
+    }
+  }
+
+  var results = [aRuby, bPython, cJS, dGo, eCs];
+  results.sort();
+  results.reverse();
+  var rank1 = results[0];
+  var rank2 = results[1];
+  var rank3 = results[2];
+
+}
+
+
+// $(document).ready(function () {
+
+// $("form#questions").submit(function(){
+//     $("#quizQuestion").tallyResults() {
+//       if (rank1 || rank2 || rank3  === aRuby) {
+//         ("#ruby").show();
+//       }
+//       else if (rank1 || rank2 || rank3  === bPython) {
+//         ("#python").show();
+//       }
+//       else if (rank1 || rank2 || rank3  === cJS) {
+//         ("#js").show();
+//       }
+//       else if (rank1 || rank2 || rank3  === dGo) {
+//         ("#go").show();
+//       }
+//       else if (rank1 || rank2 || rank3  === eCs) {
+//         ("#cs").show();
+//       }
+//       else {
+//         alert("Something went wrong!")
+//       }
+//    }
+//   });
+
+
+
+(document).ready(function () {
+  $("form#questions").submit(function () {
+
+    event.preventDefault();
+    // let rank1 = "none"
+    // let rank2 = "none"
+    // let rank3 = "none"
+
+    // // gather user input
+    // // pass the user input into the function
+    // tallyResults()
+    // // store result
+    // // output to DOM
+
+    // if ((rank1 || rank2 || rank3) === aRuby) {
+    //   ("#ruby").show();
+    // }
+    // else if ((rank1 || rank2 || rank3) === bPython) {
+    //   ("#python").show();
+    // }
+    // else if ((rank1 || rank2 || rank3) === cJS) {
+    //   ("#js").show();
+    // }
+    // else if ((rank1 || rank2 || rank3) === dGo) {
+    //   ("#go").show();
+    // }
+    // else if ((rank1 || rank2 || rank3) === eCs) {
+    //   ("#cs").show();
+    // }
+    // else {
+    //   alert("Something went wrong!")
+    // }
+
+
+    // $("form#blanks").submit(function () {
+    //   var blanks = ["userName"];
+
+    //   blanks.forEach(function (blank) {
+    //     var userInput = $("input#" + blank).val();
+    //     $("." + blank).text(userInput).val();
+
+    //   });
+
+
+    //   $("button").click(function () {
+    //     $("." + blanks).toggle();
+    //     $("." + results).toggle();
+    //   });
+
+
+  });
+});
